d6d17cd191169a39e23ffc92c8d540bb93712166
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2025-02-17 23:03:07 +0530

    update component implementation

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Skills from "./components/Skills";
 import Education from "./components/Education";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import Certifications from "./components/Certifications";
 
 const App = () => {
   return (
@@ -20,6 +21,7 @@ const App = () => {
         <Bio />
         <Skills />
         <Education />
+        <Certifications />
         <Contact />
         <Footer />
       </div>
