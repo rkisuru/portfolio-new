c4a1084bfdbb7e52c8c2c2cc1923bc8a4f16f6e7
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2025-02-17 10:39:12 +0530

    implement components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Bio from "./components/Bio";
+import Skills from "./components/Skills";
+import Education from "./components/Education";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-blue-700">Hello World !</h1>
+    <div className="relative h-full overflow-y-auto antialiased">
+      <div className="fixed inset-0 bg-fixed bg-cover bg-center bg-img"></div>
+
+      <div className="relative z-10 flex flex-col items-center p-4 space-y-8 container mx-auto">
+        <Hero />
+        <Navbar />
+        <Projects />
+        <Bio />
+        <Skills />
+        <Education />
+        <Contact />
+        <Footer />
+      </div>
     </div>
   );
 };
