538e2b7aded0f9ebdbf834ebbc042e0e5ab13428
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2025-02-17 13:40:09 +0530

    feat: implement animations (framer motion)

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,6 +2,7 @@ import emailjs from "@emailjs/browser";
 import { useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
 import { FiSend } from "react-icons/fi";
+import { motion } from "framer-motion";
 
 const Contact = () => {
   const [formData, setFormData] = useState({
@@ -73,7 +74,12 @@ const Contact = () => {
       <h2 className="my-8 text-center text-4xl font-semibold tracking-tighter">
         Let's Connect
       </h2>
-      <form onSubmit={handleSubmit}>
+      <motion.form
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.8, delay: 0.7 }}
+        onSubmit={handleSubmit}
+      >
         <div className="mb-4 flex space-x-4">
           <div className="lg:w-1/2">
             <input
@@ -86,7 +92,13 @@ const Contact = () => {
               className="mb-8 w-full appearance-none rounded-lg border border-stone-50/30 bg-transparent px-3 py-2 text-sm focus:border-stone-400 focus:outline-none"
             />
             {errors.name && (
-              <p className="text-sm text-rose-800">{errors.name}</p>
+              <motion.p
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                className="text-sm text-rose-800"
+              >
+                {errors.name}
+              </motion.p>
             )}
           </div>
           <div className="lg:w-1/2">
@@ -100,7 +112,13 @@ const Contact = () => {
               className="mb-8 w-full appearance-none rounded-lg border border-stone-50/30 bg-transparent px-3 py-2 text-sm focus:border-stone-400 focus:outline-none"
             />
             {errors.email && (
-              <p className="text-sm text-rose-800">{errors.email}</p>
+              <motion.p
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                className="text-sm text-rose-800"
+              >
+                {errors.email}
+              </motion.p>
             )}
           </div>
         </div>
@@ -116,7 +134,13 @@ const Contact = () => {
             rows="6"
           />
           {errors.message && (
-            <p className="text-sm text-rose-800">{errors.message}</p>
+            <motion.p
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              className="text-sm text-rose-800"
+            >
+              {errors.message}
+            </motion.p>
           )}
         </div>
         <button
@@ -131,7 +155,7 @@ const Contact = () => {
             <FiSend />
           </div>
         </button>
-      </form>
+      </motion.form>
     </div>
   );
 };
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { HERO } from "../constants";
 import isuru from "../assets/Isuru.jpg";
+import { motion } from "framer-motion";
 
 const Hero = () => {
   return (
     <section className="flex min-h-screen flex-wrap items-center">
-      <div className="w-full md:w-1/2">
+      <motion.div
+        initial={{ opacity: 0, x: -50 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1 }}
+        className="w-full md:w-1/2"
+      >
         <h2 className="my-8 p-2 text-4xl font-bold md:text-5xl lg:text-[7rem]">
           {HERO.name}
         </h2>
@@ -13,16 +19,24 @@ const Hero = () => {
           {HERO.greet}
         </p>
         <p className="mb-8 p-2 text-xl">{HERO.description}</p>
-      </div>
-      <div className="w-full md:w-1/2 lg:p-8">
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 1 }}
+        className="w-full md:w-1/2 lg:p-8"
+      >
         <div className="flex justify-center">
-          <img
+          <motion.img
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1 }}
             src={isuru}
             className="rounded-3xl max-h-[550px] max-w-[550px]"
             alt="Hero"
           />
         </div>
-      </div>
+      </motion.div>
     </section>
   );
 };
