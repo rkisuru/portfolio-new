2b4226454d3a40b60fe9319bcc7ca37f891c58b5
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2025-02-20 21:55:55 +0530

    change styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,16 +10,15 @@ const Hero = () => {
         initial={{ opacity: 0, x: -50 }}
         animate={{ opacity: 1, x: 0 }}
         transition={{ duration: 1 }}
-        className="w-full md:w-1/2"
+        className="w-full md:w-1/2 lg:max-w-3xl"
       >
-        <h2 className="my-8 p-2 text-4xl font-bold md:text-5xl lg:text-[7rem]">
+        <h2 className="my-8 p-2 text-4xl font-bold md:text-5xl lg:text-7xl">
           {HERO.name}
         </h2>
         <p className="p-2 text-3xl tracking-tighter lg:text-4xl">
           {HERO.greet}
         </p>
-        <p className="mb-8 p-2 text-xl">{HERO.description}</p>
-        <p className="p-2 text-lg tracking-tighter">{HERO.motto}</p>
+        <p className="mb-2 p-2 text-xl">{HERO.description}</p>
       </motion.div>
       <motion.div
         initial={{ opacity: 0, scale: 0.8 }}
@@ -27,16 +26,14 @@ const Hero = () => {
         transition={{ duration: 1 }}
         className="w-full md:w-1/2 lg:p-8"
       >
-        <div className="flex justify-center">
-          <motion.img
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 1 }}
-            src={isuru}
-            className="rounded-3xl max-h-[550px] max-w-[550px]"
-            alt="Hero"
-          />
-        </div>
+        <motion.img
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1 }}
+          src={isuru}
+          className="rounded-3xl h-[420px] mx-auto"
+          alt="Hero"
+        />
       </motion.div>
     </section>
   );
