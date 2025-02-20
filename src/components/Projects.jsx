d72937a7b6aa9be38fdcf30ca18bb2aae72344c3
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2025-02-20 23:10:22 +0530

    change styling

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -13,7 +13,7 @@ const Projects = () => {
       >
         Projects
       </motion.h2>
-      <div className="grid grid-cols-1 gap-8 md:grid-cols-2 xl:grid-cols-3">
+      <div className="hidden sm:grid grid-cols-1 gap-8 md:grid-cols-2 xl:grid-cols-3">
         {PROJECTS.map((project) => (
           <motion.div
             initial={{ opacity: 0, scale: 0.9 }}
@@ -35,6 +35,41 @@ const Projects = () => {
               transition={{ duration: 0.5 }}
               className="absolute inset-0 flex flex-col items-center justify-center text-white opacity-0 backdrop-blur-lg transition-opacity durartion-500 group-hover:opacity-100 active:opacity-100"
             >
+              <h3 className="mb-2 text-xl">{project.name}</h3>
+              <p className="mb-12 p-4">{project.description}</p>
+              <a
+                href={project.githubLink}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="px-4 py-2 text-black hover:bg-gray-300 bg-white rounded-full"
+              >
+                <div className="flex items-center">
+                  <span>View on GitHub</span>
+                  <MdArrowOutward />
+                </div>
+              </a>
+            </motion.div>
+          </motion.div>
+        ))}
+      </div>
+
+      <div className="sm:hidden grid grid-cols-1 gap-8 md:grid-cols-2 xl:grid-cols-3">
+        {PROJECTS.map((project) => (
+          <motion.div
+            initial={{ opacity: 0, scale: 0.9 }}
+            whileInView={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }}
+            whileHover={{ scale: 1.05 }}
+            key={project.id}
+            className="group relative overflow-hidden rounded-3xl"
+          >
+            <motion.img
+              whileHover={{ scale: 1.1 }}
+              src={project.image}
+              alt={project.name}
+              className="h-[400px] w-[400px] object-cover transition-transform duration-500 group-hover:scale-110"
+            />
+            <motion.div className="absolute inset-0 flex flex-col items-center justify-center text-white backdrop-blur-md durartion-500">
               <h3 className="mb-2 text-xl">{project.name}</h3>
               <p className="mb-12 p-4">{project.description}</p>
               <a
